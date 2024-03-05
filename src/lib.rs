8fa9695af55c98272a5805e43e2e7d0bf9d699da
andrewwillette <willette.andrew1846@gmail.com>
2024-03-05 14:07:24 -0600

    master: cleaning up implementation

--- src/lib.rs
+++ src/lib.rs
@@ -4,28 +4,42 @@ use std::str;
 
 /// Prompts the user to select the item in the CLI using the fzf tool.
 /// It's required that fzf is installed in the environment.
-pub fn select(fzf_input: Vec<String>, args: Vec<String>) -> String {
+pub fn select(fzf_selections: Vec<String>, fzf_cli_args: Vec<String>) -> String {
+    let mut fzf_in = String::new();
+    for selection in fzf_selections {
+        fzf_in.push_str(&selection);
+        // newline splits up the selections for fzf
+        fzf_in.push('\n');
+    }
+
     let mut child = Command::new("fzf")
-        .args(args)
+        .args(fzf_cli_args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to spawn child process");
 
-    let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    let mut fzf_in = String::new();
-    for selection in fzf_input {
-        fzf_in.push_str(&selection);
-        fzf_in.push('\n');
-    }
-    stdin
+    // write the selections to fzf command's stdin
+    child
+        .stdin
+        .take()
+        .expect("Failed to open stdin")
         .write_all(fzf_in.as_bytes())
         .expect("Failed to write fzf_input to fzf command stdin");
-    let output = child
-        .wait_with_output()
-        .expect("Failed to read fzf command stdout");
-    let mut selection = String::from(str::from_utf8(&output.stdout).unwrap());
+
+    let mut selection = String::from(
+        str::from_utf8(
+            &child
+                .wait_with_output()
+                .expect("Failed to read fzf command stdout")
+                .stdout,
+        )
+        .unwrap(),
+    );
+
+    // trime the newline we added previously from the selection
     trim_newline(&mut selection);
+
     return selection;
 }
 
@@ -46,29 +60,33 @@ mod tests {
 
     #[test]
     fn test_select() {
-        let test_input = vec!["test".to_string(), "me".to_string()];
+        let expected = "test";
+        let test_input = vec![expected.to_string(), "me".to_string()];
         let output = select(test_input, Vec::new());
-        assert_eq!("test", output);
+        assert_eq!(expected, output);
     }
 
     #[test]
     fn test_select_with_args() {
-        let test_input = vec!["test".to_string(), "me".to_string()];
+        let expected = "test";
+        let test_input = vec![expected.to_string(), "me".to_string()];
         let output = select(test_input, vec![String::from("--layout=reverse")]);
-        assert_eq!("test", output);
+        assert_eq!(expected, output);
     }
 
     #[test]
     fn test_select_with_trailing_spaces() {
-        let test_input = vec!["test ".to_string(), "me".to_string()];
+        let expected = "test ";
+        let test_input = vec![expected.to_string(), "me".to_string()];
         let output = select(test_input, Vec::new());
-        assert_eq!("test ", output);
+        assert_eq!(expected, output);
     }
 
     #[test]
     fn test_select_with_middle_spaces() {
-        let test_input = vec!["test test ".to_string(), "me".to_string()];
+        let expected = "test test ";
+        let test_input = vec![expected.to_string(), "me".to_string()];
         let output = select(test_input, Vec::new());
-        assert_eq!("test test ", output);
+        assert_eq!(expected, output);
     }
 }
