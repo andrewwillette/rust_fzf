d19ef488e6ffc9ff94d22d2212e91ee8473effe4
andrewwillette <willette.andrew1846@gmail.com>
2024-03-05 20:25:58 -0600

    master: handling missing fzf tool as Err type of Result

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,6 @@ pub fn select(
     fzf_selections: Vec<String>,
     fzf_cli_args: Vec<String>,
 ) -> Result<Vec<String>, String> {
-    if !validate_cli_tool_installed("fzf".to_string()) {
-        return Err("fzf is not installed".to_string());
-    }
-
     // we input the fzf_selections to fzf command's
     // stdin as a single string with newline separated values
     let mut fzf_in = String::new();
@@ -20,12 +16,15 @@ pub fn select(
         fzf_in.push('\n');
     }
 
-    let mut fzf_cmd: std::process::Child = Command::new("fzf")
+    let mut fzf_cmd: std::process::Child = match Command::new("fzf")
         .args(fzf_cli_args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
-        .expect("failed to spawn fzf command");
+    {
+        Ok(child) => child,
+        Err(_) => return Err("failed to spawn fzf CLI tool".to_string()),
+    };
 
     // write the selections to fzf command's stdin
     fzf_cmd
@@ -63,6 +62,7 @@ mod select_tests {
         let expected = "pickme";
         let test_input = vec![expected.to_string(), "me".to_string()];
         let output = select(test_input, Vec::new());
+        assert_eq!(true, output.is_ok());
         assert_eq!(vec![expected], output.unwrap());
     }
 
@@ -100,29 +100,5 @@ mod select_tests {
         let mut expected_sorted = vec![expected, expected2];
         expected_sorted.sort();
         assert_eq!(expected_sorted, output);
-    }
-}
-
-fn validate_cli_tool_installed(cli_tool: String) -> bool {
-    Command::new(cli_tool)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()
-        .is_ok()
-}
-
-#[cfg(test)]
-mod validate_cli_tool_installed_tests {
-    use super::*;
-    #[test]
-    fn program_not_installed() {
-        let fzf_installed = validate_cli_tool_installed("fzffff".to_string());
-        assert_eq!(false, fzf_installed);
-    }
-
-    #[test]
-    fn program_installed() {
-        let fzf_installed = validate_cli_tool_installed("fzf".to_string());
-        assert_eq!(true, fzf_installed);
     }
 }
